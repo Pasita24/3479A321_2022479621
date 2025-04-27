30bc5715665becbc63a517ca1b910ecd0356d720
Pasita <127953530+Pasita24@users.noreply.github.com>
2025-04-26 20:37:56 -0400

    Actualizacion
    Habian problemas de repositorio y se hicieron los cambios correspondiente para la importacion del nuevo proyecto

--- flutter_application_1/README.md
+++ flutter_application_1/README.md
@@ -1,4 +1,4 @@
-# flutter_application_1
+# fultter_aplication_laboratorio
 
 A new Flutter project.
 
--- flutter_application_1/android/app/src/main/kotlin/com/example/fultter_aplication_laboratorio/MainActivity.kt
+++ flutter_application_1/android/app/src/main/kotlin/com/example/fultter_aplication_laboratorio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_1
+package com.example.fultter_aplication_laboratorio
 
 import io.flutter.embedding.android.FlutterActivity
 
